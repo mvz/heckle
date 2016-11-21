c53ee58f3bb821494dc640ec32f9cc9f160ca6d2
Matijs van Zuijlen <matijs@matijs.net>
2016-11-21 07:46:06 +0100

    Make MiniTestHeckler work

--- lib/minitest_heckler.rb
+++ lib/minitest_heckler.rb
@@ -6,28 +6,16 @@ class MiniTestHeckler < Heckle
   end
 
   def tests_pass?
-    silence do
-      MiniTest::Unit.runner = nil
+    output = Heckle.debug ? $stdout: StringIO.new('')
+    reporter = Minitest::CompositeReporter.new
+    reporter << Minitest::SummaryReporter.new(output)
+    reporter << Minitest::ProgressReporter.new(output)
 
-      MiniTest::Unit.new.run
-
-      runner = MiniTest::Unit.runner
-
-      runner.failures == 0 && runner.errors == 0
+    reporter.start
+    Minitest::Runnable.runnables.each do |runnable|
+      runnable.run(reporter, {})
     end
-  end
-
-  # TODO: Windows.
-  def silence
-    return yield if Heckle.debug
-
-    begin
-      original = MiniTest::Unit.output
-      MiniTest::Unit.output = File.open("/dev/null", "w")
-
-      yield
-    ensure
-      MiniTest::Unit.output = original
-    end
+    reporter.report
+    reporter.passed?
   end
 end
--- test/test_heckle_runner.rb
+++ test/test_heckle_runner.rb
@@ -19,35 +19,35 @@ class TestHeckleRunnerRun < Minitest::Test
     @old_pwd = Dir.pwd
     Dir.chdir @@dummy_dir
 
-    # See MiniTest's test/minitest/metametameta.rb
-    @output = StringIO.new("")
-    MiniTest::Unit::TestCase.reset
-    MiniTest::Unit.output = @output
+    Minitest::Runnable.reset
   end
 
   def teardown
     super
     Dir.chdir @old_pwd
-    MiniTest::Unit.output = $stdout
 
-    MiniTest::Unit::TestCase.test_suites.each do |test|
+    Minitest::Runnable.runnables.each do |test|
       Object.send :remove_const, test.to_s.to_sym
     end
   end
 
   def test_run_with_full_coverage
+    result = nil
     out, _ = capture_io do
-      HeckleRunner.run %w[Doubler double]
+      result = HeckleRunner.run %w[Doubler double]
     end
 
+    assert result
     assert_match %r{No mutants survived.}, out
   end
 
   def test_run_with_partial_coverage
+    result = true
     out, _ = capture_io do
-      HeckleRunner.run %w[Doubler double --tests test/test_doubler_with_a_number.rb]
+     result = HeckleRunner.run %w[Doubler double --tests test/test_doubler_with_a_number.rb]
     end
 
+    refute result
     assert_match %r{The following mutations didn't cause test failures:}, out
     refute_match %{No mutants survived.}, out
   end
